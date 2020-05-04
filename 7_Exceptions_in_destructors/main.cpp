b92d8740e862c8ca5790006db7ee5a024ddb63de
Vadim <varem99@mail.ru>
2020-05-04 22:57:25 +0300

    Exception in destructor finalize

--- 7_Exceptions_in_destructors/main.cpp
+++ 7_Exceptions_in_destructors/main.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 #include <string>
-#include <memory>
+
 
 class Dog
 {
 public:
-	~Dog() { std::cout << "Dog destroyed!" << std::endl; }
-};
-
-class YellowDog: public Dog{
-public:
-	~YellowDog() { std::cout << "Yellow dog destroyed!" << std::endl; }
-};
-
-class DogFactory{
-public:
-	static std::shared_ptr<Dog> createYellowDog()
+	std::string m_name;
+	Dog(std::string name)
 	{
-		return std::shared_ptr<YellowDog>(new YellowDog());
+		m_name = name;
+		std::cout << name << " is born" << std::endl;
 	}
-	// create other dogs
+	~Dog()
+	{
+		std::cout << m_name << " is destroyed" << std::endl;
+	}
+	void prepareToDestr()
+	{
+		std::cout << "Preparation ..." << std::endl;
+		throw 10;
+	}
+	void bark()
+	{
+		std::cout << "bark!" << std::endl;
+	}
 };
+
 
 // --------------------------------------------------------------------
 
 int main()
 {
-	std::shared_ptr<Dog> pd = DogFactory::createYellowDog();
-	// Do smth with pd
+	try{
+		Dog henry("Henry");
+		Dog bob("Bob");
+		
+		henry.bark();
+		bob.bark();
+		henry.prepareToDestr();
+		bob.prepareToDestr();
+	} catch(int e){
+		std::cout << e << " is caught" << std::endl;
+	}
 	return 0;
 }
